6319707a9bd02acda5c32d54b3130adfb0d7d344
sanlake <44362005+sanlake@users.noreply.github.com>
2020-10-27 06:08:39 -0300

    Includes 01 cpp URI solutions

---
+++ uri/1036.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+	double A,B,C,delta,r1,r2;
+	
+	std::cin>>A>>B>>C;
+
+	if(A==0){std::cout<<"Impossivel calcular"<<std::endl;return 0;}
+
+	delta = (B*B)-(4*A*C);
+
+	if(delta<0){std::cout<<"Impossivel calcular"<<std::endl;return 0;}
+
+	r1 = (-B+sqrt(delta))/(2*A);
+	r2 = (-B-sqrt(delta))/(2*A);
+
+	std::cout.precision(5);
+	std::cout<<std::fixed;
+
+	std::cout<<"R1 = "<<r1<<std::endl;
+	std::cout<<"R2 = "<<r2<<std::endl;
+}
