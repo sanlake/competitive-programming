e4c642ee0e5b5136fc7c7eba2dbf301f76aff5fb
sanlake <44362005+sanlake@users.noreply.github.com>
2020-10-27 19:00:28 -0300

    Includes 01 cpp URI solutions

---
+++ uri/1038.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int32_t main(void){
+	int n,q;
+	std::vector<double> p={4.00,4.50,5.00,2.00,1.50};
+	
+	std::cin>>n>>q;
+
+	std::cout.precision(2);
+	std::cout<<std::fixed;
+	std::cout<<"Total: R$ "<<q*p[n-1]<<std::endl;
+}
