f92fcc888691d57161cc4c0de4341c1c03626fea
sanlake <44362005+sanlake@users.noreply.github.com>
2020-10-30 18:51:36 -0300

    Includes 01 cpp URI solutions

---
+++ uri/1064.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int32_t main(void){
+	int n=0;
+	double so=0.0,aux;
+
+	for(int i=0; i<6; i++){
+		std::cin>>aux;
+		if(aux>0) { n++; so+=aux; }
+	}
+
+	std::cout.precision(1);
+	std::cout<<std::fixed;
+	std::cout<<n<<" valores positivos"<<std::endl;
+	std::cout<<so/n<<std::endl;
+}
