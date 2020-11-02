817ae83d1318608bc0005eb1d62bfd82e059e4f5
sanlake <44362005+sanlake@users.noreply.github.com>
2020-11-01 22:52:56 -0300

    Includes 01 cpp URI solutions

---
+++ uri/1073.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+int32_t main(void){
+	int n;
+	std::cin>>n;
+	
+	std::cout<<std::fixed;
+	std::cout.precision(0);
+
+	for(int i=2; i<=n; i+=2) 
+		std::cout<<i<<"^2 = "<<pow(i,2)<<std::endl;
+}
