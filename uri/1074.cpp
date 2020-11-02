7a4239c1d120e297e524ddf8929ad511146f8672
sanlake <44362005+sanlake@users.noreply.github.com>
2020-11-01 22:53:12 -0300

    Includes 01 cpp URI solutions

---
+++ uri/1074.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int32_t main(void){
+	int n,x;
+	std::cin>>n;
+
+	for(int i=0; i<n; i++){
+		std::cin>>x;
+		if(x==0){
+			std::cout<<"NULL"<<std::endl;
+			continue;
+		}
+		else if(x%2==0) std::cout<<"EVEN ";
+		else std::cout<<"ODD ";
+
+		if(x>0) std::cout<<"POSITIVE";
+		else std::cout<<"NEGATIVE";
+
+		std::cout<<std::endl;
+	}
+}
