aa65c1ea0e9ce7be0962bbc58e7088725c98dc20
sanlake <44362005+sanlake@users.noreply.github.com>
2020-11-04 20:30:19 -0300

    Includes 01 cpp URI solutions

---
+++ uri/1080.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int32_t main(void){
+	int p,m=0,n;
+	for(int i=0; i<100; i++){
+		std::cin>>n;
+		if(n>m){
+			m=n;
+			p=i+1;
+		}
+	}
+
+	std::cout<<m<<std::endl<<p<<std::endl;
+}
