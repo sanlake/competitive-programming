961bf4e67d389eb237f917dc22297aa2325044b6
sanlake <44362005+sanlake@users.noreply.github.com>
2020-11-09 15:30:20 -0300

    Includes 01 cpp URI solutions

---
+++ uri/1117.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int32_t main(void){
+	int q=0;
+	double n,m=0.0;
+
+	while(true){
+		std::cin>>n;
+		if(n>=0 && n<=10){ m+=n; q++; }
+		else std::cout<<"nota invalida"<<std::endl;
+
+		if(q==2) break;
+	}
+
+	std::cout.precision(2);
+	std::cout<<std::fixed;
+	std::cout<<"media = "<<m/2.0<<std::endl;
+}
