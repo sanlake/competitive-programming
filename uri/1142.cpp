db540fe16515405bc2c7342446ee368429521d37
sanlake <44362005+sanlake@users.noreply.github.com>
2020-11-12 18:40:33 -0300

    Includes 01 cpp URI solutions

---
+++ uri/1142.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int32_t main(void){
+    int n,c=0;
+
+    std::cin>>n;
+
+    for(int i=0; i<n; i++){
+    std::cout<<++c<<" "<<++c<<" "<<++c<<" PUM"<<std::endl;
+    c++;
+    }
+}
