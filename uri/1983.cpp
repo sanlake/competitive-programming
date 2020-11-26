bd9f61d63c5cbcdbf6bf391dd00fcf4fdea99977
sanlake <44362005+sanlake@users.noreply.github.com>
2020-11-25 21:43:48 -0300

    Includes 01 cpp URI solutions

---
+++ uri/1983.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int32_t main(void){
+    int q;
+    double n=0,nM=-1;
+    std::string m,mM;
+
+    std::cin>>q;
+
+    for(int i=0; i<q; i++){
+        std::cin>>m>>n;
+        if(nM<n){ nM=n;mM=m; }
+    }
+
+    if(nM<8) std::cout<<"Minimum note not reached"<<std::endl;
+    else std::cout<<mM<<std::endl;
+}
