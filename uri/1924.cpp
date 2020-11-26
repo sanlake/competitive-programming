1e2e8948bb8d7aaf4540447ac1da7f7c6f725f26
sanlake <44362005+sanlake@users.noreply.github.com>
2020-11-25 21:42:04 -0300

    Includes 01 cpp URI solutions

---
+++ uri/1924.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int32_t main(void){
+    int n=0;
+    std::string a;
+    std::cin>>n;
+
+    for(int i=0; i<n; i++) std::cin>>a;
+    std::cout<<"Ciencia da Computacao"<<std::endl;
+}
