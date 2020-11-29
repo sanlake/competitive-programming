c77cbd802242f80fb8cbb113485e38fd94c88a26
sanlake <44362005+sanlake@users.noreply.github.com>
2020-11-29 09:29:25 -0300

    Includes 2 cpp SPOJ-BR solutions

---
+++ spoj-br/arithmetic_expressions/DIST13.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+
+int32_t main(void){
+	int x,y,X,Y;
+	std::cin>>x>>y>>X>>Y;
+	std::cout<<abs(X-x)+abs(y-Y)<<std::endl;
+}
---
+++ spoj-br/others/TEST.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int32_t main(void){
+	int a;
+	while(std::cin>>a && a!=42)
+		std::cout<<a<<std::endl;
+}
