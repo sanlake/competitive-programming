db7464511de3db8abf0b8fbb412c5731111a5e3e
sanlake <44362005+sanlake@users.noreply.github.com>
2020-11-29 08:49:10 -0300

    Includes 2 cpp SPOJ-BR solutions

---
+++ spoj-br/arithmetic_expressions/COMETA2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int32_t main(void){
+	int a;
+	std::cin>>a;
+	std::cout<<a+(86-((a%76)))<<std::endl;
+}
---
+++ spoj-br/arithmetic_expressions/PEDAGIO1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int32_t main(void){
+	int l,d,k,p;
+	std::cin>>l>>d>>k>>p;
+	std::cout<<(l*k)+((l/d)*p)<<std::endl;
+}
