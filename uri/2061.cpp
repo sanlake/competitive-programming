38dc4e8b8a95adede4a0989e06f296ccc1eba52a
sanlake <44362005+sanlake@users.noreply.github.com>
2020-11-30 23:55:47 -0300

    Includes 01 cpp URI solutions

---
+++ uri/2061.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int32_t main(void){
+  int N,M;
+  std::string a;
+
+  std::cin>>N>>M;
+
+  for(int i=0; i<M; i++){
+    std::cin>>a;
+    if(a[0]=='f') N++;
+    else N--;
+  }
+  
+  std::cout<<N<<std::endl;
+}
