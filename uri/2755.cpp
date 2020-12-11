4b3934525d04629aa0d09b008cc3929abc35742a
sanlake <44362005+sanlake@users.noreply.github.com>
2020-12-10 23:59:33 -0300

    Includes 01 cpp URI solution

---
+++ uri/2755.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int32_t main(void){
+    std::cout<<"\"Ro\'b\'er\tto\\/\""<<std::endl;
+    std::cout<<"(._.) ( l: ) ( .-. ) ( :l ) (._.)"<<std::endl;
+    std::cout<<"(^_-) (-_-) (-_^)"<<std::endl;
+    std::cout<<"(\"_\") (\'.\')"<<std::endl;
+}
