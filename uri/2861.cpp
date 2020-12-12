7e3a7971d09dc7a4fe9584f04e1e8d06c0449e56
sanlake <44362005+sanlake@users.noreply.github.com>
2020-12-11 23:45:00 -0300

    Includes 01 cpp URI solution

---
+++ uri/2861.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int32_t main(void){
+    int q;
+    std::string p;
+
+    std::cin>>q;
+    std::cin.ignore();
+
+    for(int i=0; i<q; i++){
+        getline(std::cin,p);
+	std::cout<<"gzuz"<<std::endl;
+   }
+}
