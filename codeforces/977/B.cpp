951dfccfeb6ebf968beba550d8bf12ebeae3efef
sanlake <44362005+sanlake@users.noreply.github.com>
2020-12-14 23:44:31 -0300

    Includes 01 cpp CODEFORCES solution

---
+++ codeforces/977/B.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+
+typedef std::pair<std::string,int> psi;
+
+struct cmp{
+    constexpr bool operator()(
+		    psi const& a,
+		    psi const& b)
+	    const noexcept
+	    {
+	        return a.second < b.second;
+	    }
+};
+
+int32_t main(void){
+    int n;
+    std::string fw,sw;
+    std::map<std::string,int> m;
+    std::priority_queue<psi,std::vector<psi>,cmp> pq;
+
+    std::cin>>n>>fw;
+
+    for(int i=0; i<n-1; i++){
+	sw=fw.substr(i,2);
+	if(m.count(sw)!=0) m.at(sw)++;
+	else m.insert(psi(sw,1));
+    }
+
+    for(auto x : m) pq.push(x);
+
+    std::cout<<pq.top().first<<std::endl;
+}
