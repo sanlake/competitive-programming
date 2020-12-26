9915871b6e40dc9d56fb6f505cb71660140ddfd7
sanlake <44362005+sanlake@users.noreply.github.com>
2020-12-25 23:37:45 -0300

    Updates the repo

---
+++ codeforces/988/C.cpp
@@ -0,0 +1,24 @@
+#define DESYNC std::ios_base::sync_with_stdio(false); std::cin.tie(0); std::cout.tie(0)
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+int32_t main(void){
+    DESYNC;
+    int n,k;
+    std::vector< std::vector<int> > v;
+
+    std::cin>>n;
+    for(int i=0; i<n; i++){
+        std::cin>>k;
+        for(int j=0; j<k; j++){
+            std::cin>>a;
+            std::cin>>
+        }
+    }
+
+
+
+    
+}
