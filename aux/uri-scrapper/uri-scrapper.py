e945cc6544b3a9666ab13f671d9e3fc9cb2541af
sanlake <44362005+sanlake@users.noreply.github.com>
2021-01-19 23:50:54 -0300

    Uses JSON parser at URI scrapper

---
+++ aux/uri-scrapper/uri-scrapper.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+with open('header.json', 'r') as headerFile:
+    content=headerFile.read()
+
+headers=json.loads(content)
+
+resource="https://www.urionlinejudge.com.br/judge/pt/problems/index/1"
+
+outputBuffer = []
+
+for page in range(1,12):
+    print("Scrapping ",str(page)+"/12")
+    response = requests.get(resource, headers=headers, params={'page':page})
+    soup = BeautifulSoup(response.text, "lxml")
+    lines = soup.find_all('tr',{"class":["impar","par"]})
+    print(lines)
+    for line in lines:
+        cols = line.find_all("td",{"class":["id","tiny"]})
+        if cols:
+            outputBuffer.append({"id": cols[0].a.string, "status": "solved" if cols[1].img else "nop"})
+
+for data in outputBuffer:
+    print(data)
