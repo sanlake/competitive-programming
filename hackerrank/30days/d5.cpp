0d2e12446d995394e94816aab2243fe163f76d06
sanlake <44362005+sanlake@users.noreply.github.com>
2021-03-09 23:33:11 -0300

    Includes 1 cpp HackerRank solution

---
+++ hackerrank/30days/d5.cpp
@@ -0,0 +1,10 @@
+#define DESYNC std::ios_base::sync_with_stdio(false); std::cin.tie(0); std::cout.tie(0);
+#include <iostream>
+
+int32_t main(void){ DESYNC
+    int n;
+    std::cin>>n;
+
+    for(int i=1; i<=10; i++) 
+	    std::cout<<n<<" x "<<i<<" = "<<n*i<<'\n';
+}
