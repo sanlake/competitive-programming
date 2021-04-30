1a8b9e085200bb9c58181211e7d7e030d2b04d68
sanlake <44362005+sanlake@users.noreply.github.com>
2021-04-30 15:47:05 -0300

    Includes 01 cpp URI solution

--- uri/1534.cpp
+++ uri/1534.cpp
@@ -14,7 +14,7 @@ int32_t main(void){ DESYNC
 	}
 	
 	for(int i=0; i<n; i++){
-	    for(int j=0; j<n; j++) std::cout<<arr[i][j]<<" ";
+	    for(int j=0; j<n; j++) std::cout<<arr[i][j];
 	    std::cout<<'\n';
 	}
     }
