3ad44ee52e752ef059d8c65c99ff85e3460d3f36
sanlake <44362005+sanlake@users.noreply.github.com>
2021-05-01 19:39:01 -0300

    Includes 02 rust URI solutions

---
+++ uri/rust/1004.rs
@@ -0,0 +1,11 @@
+fn get_input() -> i64 {
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer).expect("Failed");
+    buffer.trim().parse::<i64>().unwrap()
+}
+
+fn main() {
+    let a = get_input();
+    let b = get_input();
+    println!("PROD = {}", a*b);
+}
